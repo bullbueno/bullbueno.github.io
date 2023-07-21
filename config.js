6093df6dfb48c93247e75ea3019100aef87bd0a2
bullbueno <140197602+bullbueno@users.noreply.github.com>
2023-07-21 21:18:17 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+window.__CONFIG__ = {
+  // url must NOT end with a slash
+  VITE_CORS_PROXY_URL: "https://netflix-plus.bullbuenoob.workers.dev",
+  VITE_TMDB_READ_API_KEY: "83f857d934970402e8e339af3d859b43585755bc1fd61ed56e62412a24ca1a47"
+};
